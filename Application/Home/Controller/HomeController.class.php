c1f906fa2476a2bc23cd2d9584fdd6c6ed581911
albert <wujipeng@aliyun.com>
2016-12-22 18:08:34 +0800

    ...

---
+++ Application/Home/Controller/HomeController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class HomeController extends Controller
+{
+
+    public function _initialize()
+    {
+        
+    }
+}
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,10 +1,25 @@
 <?php
 namespace Home\Controller;
 
-use Think\Controller;
-
-class IndexController extends Controller
+/**
+ * 前端首页控制器
+ */
+class IndexController extends HomeController
 {
+    private $ip;
+    private $port;
+    public function _initialize()
+    {
+        parent::_initialize();
+
+        set_time_limit(0); //确保在连接客户端时不会超时
+        $this->ip   = '127.0.0.1';
+        $this->port = 1935;
+    }
+
+    /**
+     * 首页
+     */
     public function index()
     {
         echo 111;
