fd069f0d5ea476a51cb9197bf08bcd2174733fe3
Albert3306 <wujipeng@aliyun.com>
2017-02-23 14:57:43 +0800

    delete User Module

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -14,11 +14,12 @@ class LoginController extends Controller
     {
         if (IS_POST) {
             /* 检测验证码 TODO: */
-            if (APP_DEBUG == true){
+            if (APP_DEBUG == false){
                 if(!check_verify($verify)){
                     $this->error(L('LOGIN_VERIFY_ERROR'),U('Login/login'),true);
                 }
             }
+
         } else {
             $this->display();
         }
